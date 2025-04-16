4dcf098bc6354e70b38a8e931f18a310a86b8d6c
Matheus Bittencourt Azevedo <mbazevedo@rd.com.br>
2025-04-16 10:04:19 -0300

    ajuste no path do globals.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/globals.css',
+
   ],
   darkMode: 'class', // Alterado para 'class' para controle manual do tema
   theme: {
